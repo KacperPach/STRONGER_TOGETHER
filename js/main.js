7f7db07d2c240ae02800ae2f4fd1b25816388dec
kapsi1234 <39103506+kapsi1234@users.noreply.github.com>
2021-02-15 21:48:27 +0100

    kbrd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const keyD = keyboard("d");
+
 const app = new PIXI.Application({
     width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
 });
@@ -8,17 +10,16 @@ const container = new PIXI.Container();
 app.stage.addChild(container);
 
 // Create a new texture
-const texture = PIXI.Texture.from('examples/assets/bunny.png');
+const texture = PIXI.Texture.from('sprites/bunny.png');
 
 // Create a 5x5 grid of bunnies
 for (let i = 0; i < 25; i++) {
     const bunny = new PIXI.Sprite(texture);
     bunny.anchor.set(0.5);
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
+    bunny.x = 1;(i % 5) * 40;
+    bunny.y = 1;///Math.floor(i / 5) * 40;
     container.addChild(bunny);
 }
-
 // Move container to the center
 container.x = app.screen.width / 2;
 container.y = app.screen.height / 2;
@@ -27,9 +28,18 @@ container.y = app.screen.height / 2;
 container.pivot.x = container.width / 2;
 container.pivot.y = container.height / 2;
 
+
+
+
 // Listen for animate update
 app.ticker.add((delta) => {
     // rotate the container!
     // use delta to create frame-independent transform
     container.rotation -= 0.01 * delta;
+
+    if(keyD.isDown) {
+        container.x += 1*delta;
+    }
+
+    
 });
